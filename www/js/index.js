24be446e8d451abb23d86af0782a8ee5103c223b
Matt <me@mattnull.com>
2012-09-25 22:17:23 -0600

    sdgsdg

--- www/js/index.js
+++ www/js/index.js
@@ -28,100 +28,91 @@ var app = {
         // So, we must explicitly called `app.report()` instead of `this.report()`.
         app.report('deviceready');
 
-        $(window).scroll(function () { 
+        // $(window).scroll(function () { 
             
-            var scrollTop = $(window).scrollTop();
+        //     var scrollTop = $(window).scrollTop();
              
-            if(scrollTop > 0){
-                $('#wrapper').removeClass('expanded-top');
-            }else{
-                $('#wrapper').addClass('expanded-top');
-            }
+        //     if(scrollTop > 0){
+        //         $('#wrapper').removeClass('expanded-top');
+        //     }else{
+        //         $('#wrapper').addClass('expanded-top');
+        //     }
         
-        });
+        // });
 
-        var calcStrength = function(clicked){
-            // The List
-            $('.forecast-days > li').each(function(){
-                var val = $(this).find('.meta-' + clicked).text();
-                var val = val.slice(0, -1);
-                var forecast = $('.forecast-days');
-                var highData = parseInt(forecast.attr('data-temp-high'));
-                var lowData = parseInt(forecast.attr('data-temp-low'));
+        // var calcStrength = function(clicked){
+        //     // The List
+        //     $('.forecast-days > li').each(function(){
+        //         var val = $(this).find('.meta-' + clicked).text();
+        //         var val = val.slice(0, -1);
+        //         var forecast = $('.forecast-days');
+        //         var highData = parseInt(forecast.attr('data-temp-high'));
+        //         var lowData = parseInt(forecast.attr('data-temp-low'));
 
-                if(clicked == 'high') {
-                    var Str = calcAsc(val, highData);
-                } else if(clicked == 'low') {
-                    var Str = calcDsc(val, lowData);
-                } else if(clicked == 'rain') {
-                    var Str = calcPercent(val);
-                }
-                $(this).removeClass().addClass('str-' + Str);
-                $('#top-wrap').removeClass().addClass('str-' + Str);
-            });
-         }
+        //         if(clicked == 'high') {
+        //             var Str = calcAsc(val, highData);
+        //         } else if(clicked == 'low') {
+        //             var Str = calcDsc(val, lowData);
+        //         } else if(clicked == 'rain') {
+        //             var Str = calcPercent(val);
+        //         }
+        //         $(this).removeClass().addClass('str-' + Str);
+        //         $('#top-wrap').removeClass().addClass('str-' + Str);
+        //     });
+        //  }
         
-        /* MATH STUFF:  the following functions are here to find the strength to apply colors. */
+        // /* MATH STUFF:  the following functions are here to find the strength to apply colors. */
         
-            // function to clip ranges between 0 and 100
-            function clip(num) {
-                return (num > 100) ? 100 : (num < 0) ? 0 : num;
-            }
+        //     // function to clip ranges between 0 and 100
+        //     function clip(num) {
+        //         return (num > 100) ? 100 : (num < 0) ? 0 : num;
+        //     }
             
-            // return the correct strength classes function.
-            var range = 5;
-            function calcAsc(val, data) { // for highs.  highest temps highlighted
-                var Normal = data; // get from api
-                var NormalBottom = Normal - range; // bottom of range around normal temp
-                var Percent = Math.round(parseInt((((val - NormalBottom)/(range*2))*100))/5)*5;
+        //     // return the correct strength classes function.
+        //     var range = 5;
+        //     function calcAsc(val, data) { // for highs.  highest temps highlighted
+        //         var Normal = data; // get from api
+        //         var NormalBottom = Normal - range; // bottom of range around normal temp
+        //         var Percent = Math.round(parseInt((((val - NormalBottom)/(range*2))*100))/5)*5;
 
-                return clip(Percent);
-            }
-            function calcDsc(val, data) { // for lows.  coldest temps get highlghted
-                var Normal = data; // get from api
-                var NormalTop = Normal + range; // top of range around normal temp
-                var Percent = -(Math.round(parseInt((((NormalTop - val)/(range*2))*100))/5)*5);
-                return clip(Percent);
-            }
-            function calcPercent(val) { // for standard percents
-                var Percent = Math.round(parseInt(val)/5)*5;
-                return clip(Percent);
-            }
+        //         return clip(Percent);
+        //     }
+        //     function calcDsc(val, data) { // for lows.  coldest temps get highlghted
+        //         var Normal = data; // get from api
+        //         var NormalTop = Normal + range; // top of range around normal temp
+        //         var Percent = -(Math.round(parseInt((((NormalTop - val)/(range*2))*100))/5)*5);
+        //         return clip(Percent);
+        //     }
+        //     function calcPercent(val) { // for standard percents
+        //         var Percent = Math.round(parseInt(val)/5)*5;
+        //         return clip(Percent);
+        //     }
         
-            /* handling the display of stuff */
-            // change colors/focus when clicked in top header
-            $(document).on('click', '.menu-focus li', function(){
-                var clicked = $(this).text().toLowerCase();
-                $('body').removeClass().addClass('display-' + clicked);
+        //     /* handling the display of stuff */
+        //     // change colors/focus when clicked in top header
+        //     $(document).on('click', '.menu-focus li', function(){
+        //         var clicked = $(this).text().toLowerCase();
+        //         $('body').removeClass().addClass('display-' + clicked);
                 
-                calcStrength(clicked);
+        //         calcStrength(clicked);
                 
-                // the top header changes when clicked?
+        //         // the top header changes when clicked?
                  
-                $('#top-wrap').each(function(){
-                    var val = $(this).find('.meta-' + clicked).text();
-                    var val = val.slice(0, -1);
-                    if(clicked == 'high') {
-                        var Str = calcAsc(val);
-                    } else if(clicked == 'low') {
-                        var Str = calcDsc(val);
-                    } else if(clicked == 'rain') {
-                        var Str = calcPercent(val);
-                    }
-                    $(this).removeClass().addClass('str-' + Str);
-                });
+        //         $('#top-wrap').each(function(){
+        //             var val = $(this).find('.meta-' + clicked).text();
+        //             var val = val.slice(0, -1);
+        //             if(clicked == 'high') {
+        //                 var Str = calcAsc(val);
+        //             } else if(clicked == 'low') {
+        //                 var Str = calcDsc(val);
+        //             } else if(clicked == 'rain') {
+        //                 var Str = calcPercent(val);
+        //             }
+        //             $(this).removeClass().addClass('str-' + Str);
+        //         });
                 
-            });
+        //     });
             
-            /* when page loads... */
-            // The List
-            // $('.forecast-days > li').each(function(){
-            //  var val = $(this).find('.meta-high').text();
-            //  var val = val.slice(0, -1);
-            //  var Str = calcAsc(val);
-            //  $(this).removeClass().addClass('str-' + Str);
-            // });
-        
         
         // @todo.  andrew, clean up the CSS and icons to match this list.
         var iconMap = {
@@ -171,11 +162,11 @@ var app = {
             var data = {forecast : forecast, almanac : data.almanac};
             $('#forecast').html(forecastTemplate(data));
 
-            calcStrength('high');
+            //calcStrength('high');
         };
 
         //get data
-        window.navigator.geolocation.getCurrentPosition(function(pos){
+        navigator.geolocation.getCurrentPosition(function(pos){
             var lat = pos.coords.latitude;
             var lon = pos.coords.longitude;
 
